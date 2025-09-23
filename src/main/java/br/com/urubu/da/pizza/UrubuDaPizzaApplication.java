e26e1bbe25e66b15a29abb0bd2e7a863f0b4a368
Ajax Lima <ajaxlima94@gmail.com>
2025-09-22 22:21:25 -0300

    feat: inicio do projeto

---
+++ src/main/java/br/com/urubu/da/pizza/UrubuDaPizzaApplication.java
@@ -0,0 +1,13 @@
+package br.com.urubu.da.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrubuDaPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrubuDaPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/urubu/da/pizza/UrubuDaPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urubu.da.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrubuDaPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
